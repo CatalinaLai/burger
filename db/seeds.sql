801f0086329b60f370057413e5f83d424637e5b0
CatalinaLai <catalina.lai87@gmail.com>
2018-01-25 18:05:03 -0800

    burger app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Steak Burger", false);
